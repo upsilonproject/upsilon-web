33d571e0b5ba986ed559d980f3622110c260e10a
jamesread <contact@jwread.com>
2015-02-03 10:50:14 +0000

    Index now displays the first dashboard if any are defined, or a list of dashboards if there are none defined.

--- src/main/php/index.php
+++ src/main/php/index.php
@@ -1,6 +1,14 @@
 <?php
 
-$_REQUEST['id'] = 1;
-require_once 'viewDashboard.php';
+require_once 'includes/common.php';
+
+$dashboards = getDashboards();
+
+if (empty($dashboards)) {
+	require_once 'listDashboards.php';
+} else {
+	$_REQUEST['id'] = $dashboards[0]['id'];
+	require_once 'viewDashboard.php';
+}
 
 ?>
