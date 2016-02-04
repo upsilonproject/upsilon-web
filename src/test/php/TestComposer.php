59848a36bd55924d93c5ce3b056e5192ab3ed6dd
jamesread <contact@jwread.com>
2016-02-04 16:08:40 +0100

    .

---
+++ src/test/php/TestComposer.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'testsSetup.php';
+
+class TestComposer extends PHPUnit_Framework_TestCase {
+	public function testAutoloaderExists() {
+		$this->assertFalse(true);
+	}
+}
+
+?>
