6642347054760212310da2b9082462f86f7eee04
jamesread <contact@jwread.com>
2016-02-16 15:56:33 +0000

    .

--- src/test/php/testsSetup.php
+++ src/test/php/testsSetup.php
@@ -2,8 +2,8 @@
 
 set_include_path(get_include_path() . PATH_SEPARATOR . realpath(__DIR__ . '/../../main/php/'));
 
-if ((@include 'includes/libraries/autoload.php') !== false) {
-	throw new Exception('Could not include the library autoloader. Is composer up to date?');
+if ((!@include 'includes/libraries/autoload.php') !== false) {
+	throw new Exception('Could not include the library autoloader. Is composer up to date? Include path: ' . get_include_path());
 }
 
 require_once 'includes/common.php';
