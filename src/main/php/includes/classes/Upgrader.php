94ad47d312308fde55afa80467093203b8ed1e63
jamesread <contact@jwread.com>
2016-10-23 10:18:44 +0100

    Unique command arguments and stuff.

--- src/main/php/includes/classes/Upgrader.php
+++ src/main/php/includes/classes/Upgrader.php
@@ -75,6 +75,19 @@ abstract class DatabaseUpgradeTask extends UpgradeTask {
 		}
 	}
 
+	protected function tableHasUniqueKey($table, $name) {
+		$sql = 'SHOW CREATE TABLE ' . $table;
+		$stmt = stmt($sql);
+		$res = $stmt->execute()->fetchRow();
+		$res = $res['Create Table'];
+
+		if (stripos($res, "UNIQUE KEY `" . $name . "`") === FALSE) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
 	public function isPossible() {
 		return true;
 	}
@@ -174,5 +187,56 @@ class NodesTableHasConfigsColumn extends DatabaseUpgradeTask {
 }
 
 upgrader::registerTask(new NodesTableHasConfigsColumn());
+
+class RemoteConfigCommandsUnique extends DatabaseUpgradeTask {
+	public function isNecessary() {
+		if ($this->tableHasUniqueKey('remote_config_commands', 'identifier')) {
+			return false;
+		} else {
+			return true;
+		}
+
+		var_dump($res);
+
+		return true;
+	}
+
+	public function perform() {
+		$sql = 'ALTER TABLE remote_config_commands ADD UNIQUE(identifier) ';
+		stmt($sql)->execute();
+	}
+	
+	public function isPossible() {
+		return true;
+	}
+}
+
+upgrader::registerTask(new RemoteConfigCommandsUnique());
+
+class RemoteConfigCommandArgumentsUnique extends DatabaseUpgradeTask {
+	public function isNecessary() {
+		return !$this->tableHasUniqueKey('remote_config_command_arguments', 'unq_cmd_arg');
+	}
+
+	public function perform() {
+		$sql = 'ALTER TABLE remote_config_command_arguments ADD UNIQUE unq_cmd_arg (command, name)';
+		stmt($sql)->execute();
+	}
+}
+
+upgrader::registerTask(new RemoteConfigCommandArgumentsUnique());
+
+class CommandMetadataUnique extends DatabaseUpgradeTask {
+	public function isNecessary() {
+		return !$this->tableHasUniqueKey('command_metadata', 'unq_cmd');
+	}
+
+	public function perform() {
+		$sql = 'ALTER TABLE command_metadata ADD UNIQUE unq_cmd (commandIdentifier)';
+		stmt($sql)->execute();
+	}
+}
+
+upgrader::registerTask(new CommandMetadataUnique());
 
 ?>
