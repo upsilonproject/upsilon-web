0f4094e3f758174c6b5ebd7afb0d9567f7690292
jamesread <contact@jwread.com>
2017-10-09 20:00:28 +0100

    Dont warn without HTTPS by default, added a sample room

--- var/initialData.sql
+++ var/initialData.sql
@@ -1,4 +1,6 @@
 INSERT INTO settings (`key`, `value`) VALUES ('maintenanceMode', 0);
+INSERT INTO settings (`key`, `value`) VALUES ('warnNotUsingHttps', 0);
+INSERT INTO rooms (title, filename) VALUES ('sample', 'sample.svg') 
 INSERT INTO widgets (`class`) VALUES ('ListMetrics');
 INSERT INTO widgets (`class`) VALUES ('GraphMetrics');
 INSERT INTO widgets (`class`) VALUES ('ProblemServices');
@@ -9,3 +11,4 @@ INSERT INTO widgets (`class`) VALUES ('ServicesFromGroup');
 INSERT INTO widgets (`class`) VALUES ('Clock');
 INSERT INTO classes (`title`, `l`, `r`) VALUES ('Root class', 0, 1);
 INSERT INTO dashboard (`title`) VALUES ('First Dashboard');
+INSERT INTO groups (`title`, `description`) VALUES ('Staff', 'The default user group');
