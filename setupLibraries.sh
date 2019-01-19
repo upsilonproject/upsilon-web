b5eba0359f231c3a3aced1ebf11c6e167f7418b6
jamesread <contact@jwread.com>
2019-01-19 17:42:18 +0000

    Manage smarty with composer

--- setupLibraries.sh
+++ setupLibraries.sh
@@ -1,10 +1,5 @@
 echo "Setting up libraries"
 
-SMARTYDIR="src/main/php/includes/libraries/smarty"
-rm -rf $SMARTYDIR
-mkdir -p $SMARTYDIR
-tar xaf lib/smarty.tar.gz --strip 2 -C $SMARTYDIR 'smarty-*/libs/'
-
 rm -rf target/dojo-release-buildarea
 mkdir -p target/dojo-release-buildarea
 tar xaf lib/dojo-release.tgz --strip 1 -C target/dojo-release-buildarea 'dojo-release-*/'
